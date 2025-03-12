77f1181371c5d5b3ea7e8499e484021f037f73a3
Devikarajs1702 <wlover627@gmail.com>
2025-03-12 20:33:20 +0530

    Update 1.10.py

--- Tutorial1/1.10.py
+++ Tutorial1/1.10.py
@@ -1,13 +1,13 @@
-def sum_of_evens(n):
+def sum_evens(cnt):
     total = 0  
     
-    for i in range(n):
+    for i in range(cnt):
         num = int(input(f"Enter number {i+1}: "))  
         if num % 2 == 0: 
             total += num  
     
     return total 
 
-N = int(input("Enter how many numbers: "))
+cnt = int(input("Enter how many numbers: "))
 
-print(f"Sum of even numbers: {sum_of_evens(N)}")
+print(f"Sum of even numbers: {sum_evens(cnt)}")
