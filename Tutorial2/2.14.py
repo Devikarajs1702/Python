9b00f559e16202181f2acf42f3274aed56b953e8
Devikarajs1702 <wlover627@gmail.com>
2025-03-12 19:56:08 +0530

    Update 2.14.py

--- Tutorial2/2.14.py
+++ Tutorial2/2.14.py
@@ -1,23 +1,16 @@
-"""Write a python program to compute nCr using a factorial function."""
-
-
-def factorial(n):
-    if n == 0 or n == 1:
+def fact(n):
+    if n in (0, 1):
         return 1
-    fact = 1
+    res = 1
     for i in range(2, n + 1):
-        fact *= i
-    return fact
+        res *= i
+    return res
 
-def nCr(n, r):
+def comb(n, r):
     if r > n:
         return "r cannot be greater than n"
-    
-    return factorial(n) // (factorial(r) * factorial(n - r))
+    return fact(n) // (fact(r) * fact(n - r))
 
-# Get user input
 n = int(input("Enter value of n: "))
 r = int(input("Enter value of r: "))
-
-# Compute and print nCr
-print(f"nCr ({n}C{r}) = {nCr(n, r)}")
+print(f"nCr ({n}C{r}) = {comb(n, r)}")
