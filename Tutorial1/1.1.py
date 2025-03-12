861641be09a25536e3f26bb043f64cb2b96301df
Devikarajs1702 <wlover627@gmail.com>
2025-03-12 20:32:04 +0530

    Update 1.1.py

--- Tutorial1/1.1.py
+++ Tutorial1/1.1.py
@@ -1,9 +1,9 @@
-def stoh(seconds):
-    hours=seconds//3600
-    minutes=(seconds%3600)//60
-    seconds=seconds%60
-    return f"{hours:02}:{minutes:02}:{seconds:02}"
+def sec_to_hms(sec):
+    hrs = sec // 3600
+    mins = (sec % 3600) // 60
+    secs = sec % 60
+    return f"{hrs:02}:{mins:02}:{secs:02}"
 
-seconds=int(input("Enter the time in seconds: "))
-actual_time=stoh(seconds)
-print("Time in HH:MM:SS format",actual_time)
+seconds = int(input("Enter the time in seconds: "))
+formatted_time = sec_to_hms(seconds)
+print("Time in HH:MM:SS format:", formatted_time)
