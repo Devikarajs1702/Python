86ffd85526d0b39abd42a9eb3a806ca40d265047
Devikarajs1702 <wlover627@gmail.com>
2025-03-12 20:36:19 +0530

    Update 1.17.py

--- Tutorial1/1.17.py
+++ Tutorial1/1.17.py
@@ -1,6 +1,6 @@
-n = int(input("Enter the value of n: "))
+n_value = int(input("Enter the value of n: "))
 
-for i in range(1, n + 1):
-    print(f"\nMultiplication Table of {i}:") 
-    for j in range(1, 11):  
-        print(f"{i} x {j} = {i * j}") 
+for num in range(1, n_value + 1):
+    print(f"\nMultiplication Table of {num}:") 
+    for multiplier in range(1, 11):  
+        print(f"{num} x {multiplier} = {num * multiplier}")  
