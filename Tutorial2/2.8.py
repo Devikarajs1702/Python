0642396af1c17abb377c6b082b02f1176b97cc2b
Devikarajs1702 <wlover627@gmail.com>
2025-03-12 20:24:13 +0530

    Update 2.8.py

--- Tutorial2/2.8.py
+++ Tutorial2/2.8.py
@@ -1,10 +1,5 @@
-#Write a Program to replace all occurrence of a substring with a new substring.
+txt = input("Enter the string: ")
+old = input("Enter the substring: ")
+new = input("Enter the new substring: ")
 
-
-s= input("Enter the string")
-sub= input("Enter the substring ")
-sub2= input("Enter the new substring ")
-
-if sub in s:
-    s = s.replace(sub,sub2)
-    print(s)
+print(txt.replace(old, new))
