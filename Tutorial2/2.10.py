a5634cfd50793376b1ffed8819160b0c2007f9aa
Devikarajs1702 <wlover627@gmail.com>
2025-03-12 19:52:00 +0530

    Update 2.10.py

--- Tutorial2/2.10.py
+++ Tutorial2/2.10.py
@@ -1,26 +1,27 @@
+def validate_password(password):
+    has_upper = False
+    has_lower = False
+    has_digit = False
+    has_special = False
+    special_chars = "$@#"
 
-s=input("Enter the passsword ")
-hasupper=False
-haslower=False
-hasdigit=False
-hasspecial=False
-specialChar="$@#"
+    if len(password) >= 6:
+        for char in password:
+            if char.islower():
+                has_lower = True
+            elif char.isupper():
+                has_upper = True
+            elif char.isdigit():
+                has_digit = True
+            elif char in special_chars:
+                has_special = True
 
-if(len(s)>=6):
-    for i in s:
-        if i.islower():
-            haslower=True
-        elif i.isupper():
-              hasupper=True
-        elif i.isdigit():
-              hasdigit=True
-        elif i in specialChar:
-            hasspecial=True
-
-    if hasupper and haslower and hasdigit and hasspecial:
-         print(" Pasword is vlaid")
+        if has_upper and has_lower and has_digit and has_special:
+            print("Password is valid")
+        else:
+            print("Invalid password")
     else:
-         print("Invalid password")
+        print("Invalid password")
 
-else:
-    print("invalid password")                        
+user_input = input("Enter the password: ")
+validate_password(user_input)
