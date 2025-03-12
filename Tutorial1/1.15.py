80a3041597b3f5b00bca17d2cad1b917c05c6bc3
Devikarajs1702 <wlover627@gmail.com>
2025-03-12 20:35:47 +0530

    Update 1.15.py

--- Tutorial1/1.15.py
+++ Tutorial1/1.15.py
@@ -1,11 +1,11 @@
-def is_prime(n):
-    if n < 2:
+def check_prime(number):
+    if number < 2:
         return False
-    for i in range(2, int(n ** 0.5) + 1): 
-        if n % i == 0:
+    for divisor in range(2, int(number ** 0.5) + 1): 
+        if number % divisor == 0:
             return False
     return True
 
-for num in range(2, 1000):
-    if is_prime(num):
-        print(num, end=" ")
+for value in range(2, 1000):
+    if check_prime(value):
+        print(value, end=" ")
