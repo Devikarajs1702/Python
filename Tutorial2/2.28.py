feedb4f9e40d5375b2839bf4f76ee39f75d0715d
Devikarajs1702 <wlover627@gmail.com>
2025-03-12 20:14:00 +0530

    Update 2.28.py

--- Tutorial2/2.28.py
+++ Tutorial2/2.28.py
@@ -1,17 +1,13 @@
-"""Write a Python program to read a list of numbers and sort the list in a non
-decreasing order without using any built in functions. Separate function should
- be written to sort the list wherein the name of the list is passed as the parameter."""
+def sort_bubble(list):
+    size = len(list)
+    for i in range(size):
+        for j in range(0, size - i - 1):
+            if list[j] > list[j + 1]:
+                list[j], list[j + 1] = list[j + 1], list[j]
 
-def bubble_sort(lst):
-    n = len(lst)
-    for i in range(n):
-        for j in range(0, n-i-1):
-            if lst[j] > lst[j+1]:
-                lst[j], lst[j+1] = lst[j+1], lst[j]
+elem_count = int(input("Enter the number of elements: "))
+elements = [int(input(f"Enter number {i+1}: ")) for i in range(elem_count)]
 
-n = int(input("Enter the number of elements: "))
-numbers = [int(input(f"Enter number {i+1}: ")) for i in range(n)]
+sort_bubble(elements)
 
-bubble_sort(numbers)
-
-print(f"\nSorted list in non-decreasing order: {numbers}")
+print(f"\nSorted list in non-decreasing order: {elements}")
