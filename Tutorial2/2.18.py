ced6ecccf1dab4a499ede2627d9ac2688ffea41d
Devikarajs1702 <wlover627@gmail.com>
2025-03-12 20:00:46 +0530

    Update 2.18.py

--- Tutorial2/2.18.py
+++ Tutorial2/2.18.py
@@ -1,15 +1,11 @@
-""" Write a Python program to print the factorial of a number using recursion"""
-
-def factorial(n):
+def fact(n):
     if n == 0 or n == 1:
         return 1
-    return n * factorial(n - 1)
+    return n * fact(n - 1)
 
-# Get user input
-num = int(input("Enter a number: "))
+num_input = int(input("Enter a number: "))
 
-# Validate input
-if num < 0:
+if num_input < 0:
     print("Factorial is not defined for negative numbers.")
 else:
-    print(f"Factorial of {num} is {factorial(num)}")
+    print(f"Factorial of {num_input} is {fact(num_input)}")
