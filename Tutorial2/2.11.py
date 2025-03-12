f7276201222967c762c75870cae9a02f921dc29b
Devikarajs1702 <wlover627@gmail.com>
2025-03-12 19:53:44 +0530

    Update 2.11.py

--- Tutorial2/2.11.py
+++ Tutorial2/2.11.py
@@ -1,22 +1,16 @@
-"""Write Python script for converting decimal number into Binary number."""
+def dec_to_bin(n):
+    bin_str = ""
 
-
-
-
-def decimal_to_binary(n):
-    binary = ""
-    
     if n == 0:
         return "0"
-    
+
     while n > 0:
-        binary = str(n % 2) + binary  
-        n = n // 2 
-        print(binary)
-    
-    return binary
+        bin_str = str(n % 2) + bin_str  
+        n //= 2
+        print(bin_str)
+
+    return bin_str
 
 
-decimal = int(input("Enter a decimal number: "))
-
-print(f"Binary representation: {decimal_to_binary(decimal)}")
+num = int(input("Enter a decimal number: "))
+print(f"Binary representation: {dec_to_bin(num)}")
